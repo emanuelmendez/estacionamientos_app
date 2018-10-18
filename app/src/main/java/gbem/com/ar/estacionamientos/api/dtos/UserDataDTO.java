8884af96e29ba1ad6fa320ec1b47a03d6af428f7
Emanuel Mendez <pielreloj@gmail.com>
2018-10-17 22:05:10 -0300

    Cambios en DTOs

--- app/src/main/java/gbem/com/ar/estacionamientos/api/dtos/UserDataDTO.java
+++ app/src/main/java/gbem/com/ar/estacionamientos/api/dtos/UserDataDTO.java
@@ -1,6 +1,9 @@
 package gbem.com.ar.estacionamientos.api.dtos;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author pielreloj
@@ -8,12 +11,26 @@ import java.io.Serializable;
  */
 public final class UserDataDTO implements Serializable {
 
+    private static final long serialVersionUID = 8633041204967052854L;
+    private long id;
     private String username;
     private String email;
+    private String phone;
     private String name;
     private String surname;
-    private boolean hasVehicles;
-    private boolean hasParkingLots;
+    private Date since;
+    private Date lastUpdated;
+    private List<VehicleDTO> vehicles;
+    private List<ParkingLotResultDTO> parkingLots;
+    private boolean active;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
@@ -29,6 +46,14 @@ public final class UserDataDTO implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getName() {
@@ -47,28 +72,52 @@ public final class UserDataDTO implements Serializable {
         this.surname = surname;
     }
 
-    public boolean isHasVehicles() {
-        return hasVehicles;
+    public Date getSince() {
+        return since;
     }
 
-    public void setHasVehicles(boolean hasVehicles) {
-        this.hasVehicles = hasVehicles;
+    public void setSince(Date since) {
+        this.since = since;
     }
 
-    public boolean isHasParkingLots() {
-        return hasParkingLots;
+    public Date getLastUpdated() {
+        return lastUpdated;
     }
 
-    public void setHasParkingLots(boolean hasParkingLots) {
-        this.hasParkingLots = hasParkingLots;
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public List<VehicleDTO> getVehicles() {
+        return vehicles == null ? new ArrayList<>() : vehicles;
+    }
+
+    public void setVehicles(List<VehicleDTO> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    public List<ParkingLotResultDTO> getParkingLots() {
+        return parkingLots == null ? new ArrayList<>() : parkingLots;
+    }
+
+    public void setParkingLots(List<ParkingLotResultDTO> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     @Override
     public String toString() {
         return "UserDataDTO{" +
                 "username='" + username + '\'' +
-                ", hasVehicles=" + hasVehicles +
-                ", hasParkingLots=" + hasParkingLots +
+                ", hasVehicles=" + vehicles.size() +
+                ", hasParkingLots=" + parkingLots.size() +
                 '}';
     }
 }
